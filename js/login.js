5fe6a1a4f9c3948c1fcfdbe46ced506c46274eed
BKkom <79674766+bakkomcoder@users.noreply.github.com>
2023-04-30 11:39:39 +0900

    CSS
    update styles

--- js/login.js
+++ js/login.js
@@ -1,9 +1,23 @@
 const loginDiv = document.querySelector(".login");
 const loginForm = document.querySelector(".login-form");
-const loginInput = document.querySelector(".login-form input:last-child");
+const loginInput = document.querySelector(".login-form__input");
+const greeting = document.querySelector(".greeting");
 
-const loginInputBlock = document.createElement("h1");
-loginDiv.appendChild(loginInputBlock);
+// const loginInputBlock = document.createElement("h1");
+// loginDiv.appendChild(loginInputBlock);
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add("hidden");
+  const loginInputValue = loginInput.value;
+  localStorage.setItem("username", loginInputValue);
+  paintGreetings(loginInputValue);
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello, ${username}`;
+  greeting.classList.remove("hidden");
+}
 
 const savedInputValue = localStorage.getItem("username");
 
@@ -11,22 +25,5 @@ if (savedInputValue === null) {
   loginForm.classList.remove("hidden");
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  function paintGreetings(savedInputValue) {
-    loginInputBlock.innerText = `Hello, ${savedInputValue}`;
-    loginInputBlock.classList.remove("hidden");
-  }
   paintGreetings(savedInputValue);
 }
-
-function onLoginSubmit(event) {
-  event.preventDefault();
-  loginForm.classList.add("hidden");
-  const loginInputValue = loginInput.value;
-  localStorage.setItem("username", loginInputValue);
-  function paintGreetings(loginInputValue) {
-    loginInputBlock.innerText = `Hello, ${loginInputValue}`;
-    loginInputBlock.classList.remove("hidden");
-  }
-  paintGreetings(loginInputValue);
-}
-// 함수 정의 후 실행을 위해 한 번 더 작성
