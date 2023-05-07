8720160db4778fe044e97eb3d05474ba91f4ad89
BKkom <79674766+bakkomcoder@users.noreply.github.com>
2023-05-07 19:26:37 +0900

    Start Refactoring
    weather icon 추가

--- js/weather.js
+++ js/weather.js
@@ -1,19 +1,23 @@
-const weather = document.querySelector(".weather span:first-child");
-const city = document.querySelector(".weather span:last-child");
 const API_KEY = "ebe721e17481d7433057d40a316eea8a";
+const weatherIcon = document.querySelector(".weather__img");
+const weatherText = document.querySelector(".weather__text");
 
 function success(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-  console.log(url);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      weather.innerText = `${data.weather[0].main} / ${(
-        data.main.temp - 273.15
-      ).toFixed(2)}°C`;
-      city.innerText = data.name;
+      const temp = data.main.temp;
+      const weather = data.weather[0].main;
+      const city = data.name;
+      const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      weatherIcon.innerHTML = `
+        <img src=${iconUrl} />
+        <span>${city}</span>
+        <span>${weather} / ${temp}°C</span>
+      `;
     });
 }
 
